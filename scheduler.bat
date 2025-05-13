e90748c46269f20bd7fe1070d59fa90268fc3bd1
Ashrahx <elfudanshi5@gmail.com>
2025-05-12 19:47:43 -0600

    Update

--- scheduler.bat
+++ scheduler.bat
@@ -1,7 +1,7 @@
 @echo off
 SET PYTHON_PATH="C:\Python39\python.exe"
-SET SCRIPT_PATH="%~dp0src\etl_process.py"
+SET SCRIPT_PATH="%~dp0main.py"
 
-SCHTASKS /CREATE /SC DAILY /TN "PokemonETL" /TR "%PYTHON_PATH% %SCRIPT_PATH%" /ST 02:00 /RL HIGHEST
+SCHTASKS /CREATE /SC DAILY /TN "StarWarsETL" /TR "%PYTHON_PATH% %SCRIPT_PATH%" /ST 02:00 /RL HIGHEST
 echo Tarea programada para ejecutarse diariamente a las 2:00 AM
 pause
